bfcf2c5b1656c586b9bab90011ad0ba5500d167e
ilzu1 <ilzu1@yandex.ru>
2017-10-30 19:04:58 +0300

    group descrition modyfied, generate photo gallery begined

---
+++ PhotoPageGenrate.py
@@ -0,0 +1,27 @@
+page_pattern = """
+<div style = "heigh: 654px;">
+<a href = "%s"><img src = "%s" width = 872px></a>
+</div>
+%s
+"""
+
+tray_pattern = """
+<div style = "height: 66px; margin-top:10px;">
+%s
+</div>
+"""
+
+tray_elemnt_pattern = """
+<div style = "margin-left:%;">
+<a href = "%s">
+<img src = "%s" width = 88px height = 66px>
+</a>
+</div>
+"""
+
+active_tray_element_pattern = """
+<div style = "margin-left:%;">
+<img src = "%s" width = 88px height = 66px>
+</div>
+"""
+
