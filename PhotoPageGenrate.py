8c82f37af875420dbcb21cdbc6010a80f81ae52b
ilyaz88 <ilzu1@yandex.ru>
2017-11-03 20:15:09 +0300

    galery appended

--- PhotoPageGenrate.py
+++ PhotoPageGenrate.py
@@ -1,12 +1,13 @@
 # coding: utf8
-import Search4tag
+
+from serv import *
+
 class PhotoPageGenerate:
-    def __init__(self, list_fname, proto_page, pict_dir, photo_dir, pages_dir = '', preview_postfix = '', page_prefix = 'galery'):
+    def __init__(self, pict_list, pict_dir, photo_dir, pages_dir = '', preview_postfix = '', page_prefix = 'galery'):
         self.div_pattern = """
-<div style = "heigh: 654px;">
+<div style = "height: 588px; border-bottom-width: 3px; border-color: #20b2aa; border-bottom-style: solid; margin-right: 12px;">
 <a href = "%s"><img src = "%s" width = 872px></a>
 </div>
-%s
 """
         self.tray_pattern = """
 <div style = "height: 66px; margin-top:10px;">
@@ -14,55 +15,76 @@ class PhotoPageGenerate:
 </div>
 """
         self.tray_elemnt_pattern = """
-<div style = "margin-left:%s;">
+<div style = "margin-left:%s; float: left;">
 <a href = "%s">
 <img src = "%s" width = 88px height = 66px>
 </a>
 </div>
 """
         self.active_tray_element_pattern = """
-<div style = "margin-left:%s;">
+<div style = "margin-left: %s; float: left;">
 <img src = "%s" width = 88px height = 66px>
 </div>
 """
-        self.pict_list = self.GetPictList(list_fname);
-        self.proto_page = proto_page;
+        self.pict_list = pict_list;
         self.preview_magrin_left = ['0px', '10px', '10px', '10px', '10px', '10px', '10px', '10px', '10px'];
         self.pict_dir = pict_dir;
         self.photo_dir = photo_dir;
         self.preview_postfix = preview_postfix;
         self.pages_dir = pages_dir;
         self.page_prefix = page_prefix;
-        self.pasre_tag = '!paste_image_here';
+        self.page_pattern = """
+<html>
+    <head>
+        <meta charset="utf-8">        
+        <title>Галерея</title>
+        <!menu_item>Галерея<!/menu_item>
+    </head>
+    <body>
+        %s
+        %s
+    </body>
+</html>
+"""
         
     def PrewiewNameGen(self, preview_number):
         return self.pict_dir + self.pict_list[preview_number][0] + self.preview_postfix + '.jpg';
     
     def PageNameGen(self, image_number):
-        return self.pages_dir + self.page_prefix + pict_list[preview_number][0] + '.html';
+        if image_number > 0:
+            return self.pages_dir + self.page_prefix + self.pict_list[image_number][0] + '.html';
+        else:
+            return self.pages_dir + WebpageNameGenerator(self.page_prefix);
         
     def ImageNameGen(self, image_number):
-        return self.photo_dir + self.page_prefix + pict_list[preview_number][0] + '.html';
+        return self.photo_dir + self.pict_list[image_number][0] + '.jpg';
     
     def GeneratePreviewBar(self, preview_number):
-        item_row = preview_number//3;
-        item_column = preview_number%3;
+        item_row = preview_number//9;
+        item_column = preview_number%9;
         bar_description_text = '';
-        for row = range(3):
+        for row in range(3):
             line_descrition_text = '';
-            for column = range(9):                
-                if (item_row == row)&&(item_column == column):
-                    line_descrition_text = line_descrition_text + self.active_tray_element_pattern%((self.preview_magrin_left[column]), PrewiewNameGen(preview_number));
+            for column in range(9):                
+                if (item_row == row) and (item_column == column):
+                    line_descrition_text = line_descrition_text + self.active_tray_element_pattern%((self.preview_magrin_left[column]), self.PrewiewNameGen(preview_number));
                 else:
-                    line_descrition_text = line_descrition_text + self.tray_element_pattern%((self.preview_magrin_left[column]), PrewiewNameGen(preview_number));
+                    line_descrition_text = line_descrition_text + self.tray_elemnt_pattern%(self.preview_magrin_left[column], (self.PageNameGen(column+9*row)), self.PrewiewNameGen(column+9*row));
             bar_description_text = bar_description_text + self.tray_pattern%(line_descrition_text);
         return bar_description_text
             
     def GenerateImageDiv(self, image_number):
-        return self.div_pattern%(PageNameGen((image_number + 1)%27), ImageNameGen(image_number));
+        return self.div_pattern%(self.PageNameGen((image_number + 1)%27), self.ImageNameGen(image_number));
         
     def GeneratePage(self, image_number):
-        page_text =  
+        page_text = self.page_pattern%(self.GenerateImageDiv(image_number), self.GeneratePreviewBar(image_number));
+        return page_text;
         
-def GeneratePage(dname, index):
-    
+    def Do(self):
+        pages = [];
+        fname_list = [];
+        for i in range(27):
+            pages.append(self.GeneratePage(i));
+            fname_list.append(self.PageNameGen(i));
+        return self.page_prefix, pages, fname_list;
+            
--- makesite.py
+++ makesite.py
@@ -2,8 +2,9 @@
 from search4tag import search4tag
 from serv import *
 from DiaryMark import DiaryMark
+from PhotoPageGenrate import PhotoPageGenerate
 
-copyright = '&copy; stubaitrekking.ru, 2017 г., mail to <a href = "mailto:stubaitrekking@yandex.ru">stubaitrekking@yandex.ru</a>';
+copyright = '&copy; гр. А. Медведева, Нижегородский горный клуб, 2017 г., mail to <a href = "mailto:stubaitrekking@yandex.ru">stubaitrekking@yandex.ru</a>';
         
 def TmpSave(text, fname = None):
     if fname is None:
@@ -14,7 +15,37 @@ def TmpSave(text, fname = None):
 
 intro_text, diary_index_prototype_name, chapters_texts, chapters_file_names = DiaryMark();
 
-menu = [[diary_index_prototype_name], ['stubai'], ['verona'], ['route', 'path_classification', 'signalgipfel', 'gamsspitzl',  'seescharte', 'beiljoch', 'grosser'], ['tirol_transport', 'transport', 'passage'], ['proviant', 'proviant_info', 'daily_menue', 'recipe'], ['equipment', 'gas'], ['money'], ['groupe'], ['links']];
+galery_list = [['07',''],
+    ['09',''],
+    ['12',''],
+    ['14',''],
+    ['20',''],
+    ['22',''],
+    ['24',''],
+    ['25',''],    
+    ['33',''],
+    ['37',''],
+    ['40',''],
+    ['43',''],
+    ['44',''],
+    ['47',''],
+    ['48',''],
+    ['49',''],
+    ['51',''],
+    ['54',''],
+    ['57',''],
+    ['63',''],    
+    ['66',''],
+    ['67',''],
+    ['68',''],
+    ['69',''],
+    ['80',''],
+    ['86',''],
+    ['88','']];
+    
+galery_main, galery_pages, galery_fname_list = PhotoPageGenerate(galery_list, 'image/galery/preview/', 'image/galery/').Do();
+TmpSave(galery_pages[0], SourseNameGenerator(galery_main));
+menu = [[diary_index_prototype_name], ['stubai'], ['verona'], ['route', 'path_classification', 'signalgipfel', 'gamsspitzl',  'seescharte', 'beiljoch', 'grosser'], ['tirol_transport', 'transport', 'passage'], ['proviant', 'proviant_info', 'daily_menue', 'recipe'], ['equipment', 'gas'], ['money'], ['groupe'], [galery_main], ['links']];
 menu_items = [];
 
 TmpSave(intro_text, SourseNameGenerator(diary_index_prototype_name));
@@ -87,7 +118,11 @@ def PageProcess(pattern_name, sourse_name, page_name, print_name = '', menu_inde
     p.write(resulted_text)
     p.close();
 
-for menu_index in range(1, len(menu)):    
+exception = {9};
+    
+for menu_index in range(1, len(menu)):
+    if menu_index in exception:
+        continue;
     for sub_menu_index in range(0, len(menu[menu_index])):
         name = menu[menu_index][sub_menu_index];
         print('%s processing...'%name)
@@ -104,3 +139,10 @@ PageProcess('pattern.printer.html', SourseNameGenerator('diary'), PrintNameGener
 for diary_chapter_index in range(len(chapters_file_names)):
     TmpSave(chapters_texts[diary_chapter_index]);
     PageProcess('pattern.diary.html', 'tmp', chapters_file_names[diary_chapter_index], PrintNameGenerator(diary_index_prototype_name), [0, 0]);
+    
+for i in range(1, 27):
+    TmpSave(galery_pages[i]);
+    PageProcess('pattern_galery.html', 'tmp', galery_fname_list[i], '', [9, 0]);
+
+PageProcess('pattern_galery.html', SourseNameGenerator(galery_main), galery_fname_list[0], '', [9, 0]);
+    
